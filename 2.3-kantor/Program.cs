44e7537f8bea907d6a2654b38d56540fb6d1a1fb
Dawid Spisak <dawidspisak@gmail.com>
2023-11-26 16:05:46 +0100

    feat: kantor

---
+++ 2.3-kantor/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class Kantor
+{
+    static void Main(string[] args)
+    {
+        CalculateCurrency();
+    }
+
+    static void CalculateCurrency()
+    {
+        double CHF = 1.12;
+        double USD = 3.99;
+        double EUR = 1.08;
+        double GBP = 1.24;
+
+        Console.WriteLine("Enter amount of PLN:");
+        double plnAmount = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter currency code (CHF, EUR, GBP):");
+        string currencyCode = Console.ReadLine();
+
+        double result = 0;
+
+        switch (currencyCode)
+        {
+            case "CHF":
+                result = Math.Round(plnAmount / USD * CHF, 2);
+                break;
+            case "EUR":
+                result = Math.Round(plnAmount / USD * EUR, 2);
+                break;
+            case "GBP":
+                result = Math.Round(plnAmount / USD * GBP, 2);
+                break;
+            default:
+                Console.WriteLine("Wrong currency code");
+                return;
+        }
+
+        Console.WriteLine("Converted amount: " + result);
+    }
+}
