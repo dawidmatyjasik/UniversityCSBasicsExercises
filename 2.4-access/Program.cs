1329a034c7561929431ec8b6cf79d7cf02f10f67
Dawid Spisak <dawidspisak@gmail.com>
2023-11-26 16:06:22 +0100

    feat: access

---
+++ 2.4-access/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Access
+{
+    static void Main(string[] args)
+    {
+        VerifyAge();
+    }
+
+    static void VerifyAge()
+    {
+        Console.WriteLine("Enter your age:");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        if (age >= 18)
+        {
+            Console.WriteLine("Access granted");
+        }
+        else if (age >= 15 && age < 18)
+        {
+            Console.WriteLine("Do you have parental consent? (yes/no)");
+            string consent = Console.ReadLine();
+
+            if (consent.ToLower() == "yes")
+            {
+                Console.WriteLine("Access granted");
+            }
+            else
+            {
+                Console.WriteLine("Access denied");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Access denied");
+        }
+    }
+}
