6660cafa4cb80d6e54abcfcddccfa466b39ab9c1
Dawid Spisak <dawidspisak@gmail.com>
2023-11-26 16:05:20 +0100

    feat: pitagoras

---
+++ 2.1-pitagoras/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Excercises;
+
+public class Pitagoras
+{
+    static void Main(string[] args)
+    {
+        CalculateHypotenuse(3.5, 6);
+    }
+    static void CalculateHypotenuse(double a, double b)
+    {
+        double result = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+        Console.WriteLine(result);
+    }
+}
