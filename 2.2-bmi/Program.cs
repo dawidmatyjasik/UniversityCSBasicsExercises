6680fcb43015aa3e6272426f2489329d7dd5d2c7
Dawid Spisak <dawidspisak@gmail.com>
2023-11-26 16:05:33 +0100

    feat: bmi

---
+++ 2.2-bmi/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Bmi
+{
+    static void Main(string[] args)
+    {
+        CalculateBmi();
+    }
+    static void CalculateBmi()
+    {
+        double weight, height;
+        Console.WriteLine("Enter your height in meters:");
+        height = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter your weight in kg:");
+        weight = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Your BMI is:");
+        double result = weight / Math.Pow(height, 2);
+        Console.WriteLine(result);
+    }
+}
